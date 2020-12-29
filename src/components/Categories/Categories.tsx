ee1c13ed7f07896cb6a0f356780117470c57cf77
Scott Solmonson <ssolmonson63@gmail.com>
2020-12-29 13:49:33 -0500

    Add structure for Categories page

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEvent, useState, FormEvent } from 'react';
+import { useAppState } from '../../state';
+
+import Button from '@material-ui/core/Button';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import Grid from '@material-ui/core/Grid';
+import { ReactComponent as GoogleLogo } from './google-logo.svg';
+import { InputLabel, Theme } from '@material-ui/core';
+import IntroContainer from '../IntroContainer/IntroContainer';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { useLocation, useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) => ({}));
+
+export default function CategoriesPage() {
+  return (
+    <CategoryContainer>
+      <Grid container justify="center"></Grid>
+    </CategoryContainer>
+  );
+}
