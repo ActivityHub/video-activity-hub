af14d3a8ed410404f6939db8feb16d8f6af8da2b
Scott Solmonson <ssolmonson63@gmail.com>
2020-12-29 16:15:58 -0500

    Add hover display for each category
    When a category containing the image is moused over, a black opacity
    background and the Title for the category will display.

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -6,19 +6,101 @@ import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import Grid from '@material-ui/core/Grid';
 import { ReactComponent as GoogleLogo } from './google-logo.svg';
 import { InputLabel, Theme } from '@material-ui/core';
-import IntroContainer from '../IntroContainer/IntroContainer';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { useLocation, useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) => ({}));
 
+const Title = styled.h2({
+  position: 'center',
+  textTransform: 'uppercase',
+  fontFamily: 'Helvetica',
+});
+
+const CategoryCol = styled.div({
+  [`:hover ${Col}`]: {
+    backgroundColor: 'rgba(0,0,0,.5)',
+  },
+  [`:hover ${Title}`]: {
+    transform: 'translate3d(0,0,0)',
+  },
+  [`:hover ${Hover}`]: {
+    opacity: 1,
+  },
+});
+
+const CookingImage = styled.div({});
+const PhotoImage = styled.div({});
+const FitnessImage = styled.div({});
+const ArtistImage = styled.div({});
+const MusicImage = styled.div({});
+const GamingImage = styled.div({});
+const MixologyImage = styled.div({});
+const DanceImage = styled.div({});
+
 export default function CategoriesPage() {
+  // function hoverHandler(e) {
+  //   e.target.style.
+  // }
+
   return (
-    <CategoryContainer>
-      <Grid container justify="center"></Grid>
-    </CategoryContainer>
+    <Container>
+      <Row>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Art</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Music</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Photography</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Cooking</Title>
+          </Hover>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Dance</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Mixology</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Gaming</Title>
+          </Hover>
+        </Col>
+        <Col>
+          <Image src="" rounded />
+          <Hover>
+            <Title>Fitness</Title>
+          </Hover>
+        </Col>
+      </Row>
+      <Button>View All Categories</Button>
+      <Button>Done</Button>
+    </Container>
   );
 }
