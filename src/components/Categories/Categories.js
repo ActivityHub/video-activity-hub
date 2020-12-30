11f65e709048a7140cfae9a4b06299f7369a18d3
Scott Solmonson <ssolmonson63@gmail.com>
2020-12-30 13:56:07 -0500

    Add descriptions to images and center heading
    1. Centered words on images to give descriptions to each category.
    These are to be implemented as a hover text in a future update.
    2. Centered heading and stylized text based off the figma design
    provided by the UX team.
    3. Edited button size and text according to figma design.

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -22,71 +22,74 @@ const CategoriesPage = () => {
       <div className="category-container">
         <div className="category-heading">
           <h2>Let's Dig Deeper</h2>
-          <h6>Select 5+ interests and we will suggest exciting experiences you can be part of</h6>
+          <p>
+            Select&nbsp;<span className="heading-interests">5+ interests</span>&nbsp;and we will suggest exciting
+            experiences you can be part of
+          </p>
         </div>
         <div className="category-pictures">
           <div className="cat-col">
             <div className="art">
-              <Image src={art} rounded width="150" height="150" alt="Art" className="art-image" />
+              <Image src={art} rounded width="250" height="256" alt="Art" className="art-image" />
               <div className="image-overlay">
-                <div className="image-text">ART</div>
+                <h4 className="image-text">ART</h4>
               </div>
             </div>
             <div className="music">
-              <Image src={music} rounded width="150" height="150" alt="Music" className="music-image" />
+              <Image src={music} rounded width="250" height="256" alt="Music" className="music-image" />
               <div className="image-overlay">
-                <div className="image-text">MUSIC</div>
+                <h4 className="image-text">MUSIC</h4>
               </div>
             </div>
           </div>
           <div className="cat-col">
             <div className="photo">
-              <Image src={photo} rounded width="150" height="150" alt="Photo" className="photo-image" />
+              <Image src={photo} rounded width="250" height="256" alt="Photo" className="photo-image" />
               <div className="image-overlay">
-                <div className="image-text">PHOTO</div>
+                <h4 className="image-text">PHOTO</h4>
               </div>
             </div>
             <div className="cooking">
-              <Image src={cooking} rounded width="150" height="150" alt="Cooking" className="cooking-image" />
+              <Image src={cooking} rounded width="250" height="256" alt="Cooking" className="cooking-image" />
               <div className="image-overlay">
-                <div className="image-text">COOKING</div>
+                <h4 className="image-text">COOKING</h4>
               </div>
             </div>
           </div>
           <div className="cat-col">
             <div className="dance">
-              <Image src={dance} rounded width="150" height="150" alt="Dance" className="dance-image" />
+              <Image src={dance} rounded width="250" height="256" alt="Dance" className="dance-image" />
               <div className="image-overlay">
-                <div className="image-text">DANCE</div>
+                <h4 className="image-text">DANCE</h4>
               </div>
             </div>
             <div className="mixology">
-              <Image src={mixology} rounded width="150" height="150" alt="Mixology" className="mixology-image" />
+              <Image src={mixology} rounded width="250" height="256" alt="Mixology" className="mixology-image" />
               <div className="image-overlay">
-                <div className="image-text">MIXOLOGY</div>
+                <h4 className="image-text">MIXOLOGY</h4>
               </div>
             </div>
           </div>
           <div className="cat-col">
             <div className="gaming">
-              <Image src={gaming} rounded width="150" height="150" alt="Gaming" className="gaming-image" />
+              <Image src={gaming} rounded width="250" height="256" alt="Gaming" className="gaming-image" />
               <div className="image-overlay">
-                <div className="image-text">GAMING</div>
+                <h4 className="image-text">GAMING</h4>
               </div>
             </div>
             <div className="fitness">
-              <Image src={fitness} rounded width="150" height="150" alt="Fitness" className="fitness-image" />
+              <Image src={fitness} rounded width="250" height="256" alt="Fitness" className="fitness-image" />
               <div className="image-overlay">
-                <div className="image-text">FITNESS</div>
+                <h4 className="image-text">FITNESS</h4>
               </div>
             </div>
           </div>
         </div>
         <div className="category-btn">
-          <Button variant="primary" className="viewall-btn">
+          <Button variant="secondary" className="viewall-btn">
             View all categories
           </Button>
-          <Button variant="secondary" className="done-btn">
+          <Button variant="primary" className="done-btn">
             Done
           </Button>
         </div>
