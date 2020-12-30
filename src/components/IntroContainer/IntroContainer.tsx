c35c8186f745be319b2a7fafbce0692939b2934c
Sebastian <sebastian.chang23@gmail.com>
2020-12-29 19:01:56 -0600

    Added firebase info.  Started Shareable links

--- src/components/IntroContainer/IntroContainer.tsx
+++ src/components/IntroContainer/IntroContainer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles, Theme, Typography } from '@material-ui/core';
 import Swoosh from './swoosh';
 import VideoLogo from './VideoLogo';
-import TwilioLogo from './TwilioLogo';
 import { useAppState } from '../../state';
 import UserMenu from './UserMenu/UserMenu';
 import { useLocation } from 'react-router-dom';
