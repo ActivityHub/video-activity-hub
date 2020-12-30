f9bd46d1e6803a7beb301f735c90d53aba333580
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-12-30 18:50:01 +0000

    Cards added

--- src/components/MainActivities/MainActivities.js
+++ src/components/MainActivities/MainActivities.js
@@ -3,10 +3,18 @@ import './MainActivities.scss';
 
 import Avatar from 'react-avatar';
 import { Card, CardDeck, Button } from 'react-bootstrap';
-// import background from '../../images/homepage-bkg.png';
-// import camera from '../../images/homepage-camera.png';
-// import pottery from '../../images/homepage-pottery.png';
-// import oldYoung from '../../images/homepage-oldyoung.png';
+import baking from '../../images/mainactivities-baking.png';
+import broadway from '../../images/mainactivities-broadway.png';
+import cocktail from '../../images/mainactivities-cocktail.png';
+import cocktail1 from '../../images/mainactivities-cocktail1.png';
+import drag from '../../images/mainactivities-drag.png';
+import french from '../../images/mainactivities-french.png';
+import guitar from '../../images/mainactivities-guitar.png';
+import pasta from '../../images/mainactivities-pasta.png';
+import piano from '../../images/mainactivities-piano.png';
+import rock from '../../images/mainactivities-rock.png';
+import salad from '../../images/mainactivities-salad.png';
+import santorini from '../../images/mainactivities-santorini.png';
 
 const MainActivities = () => {
   return (
@@ -29,41 +37,37 @@ const MainActivities = () => {
         <h2>This Week's Events</h2>
         <div className="MainActivities-Weekly-Container">
           <CardDeck>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={piano} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This
-                  content is a little bit longer.
-                </Card.Text>
+                <Card.Title>Piano Classics</Card.Title>
+                <Card.Text className="date">Jan 7, 2020</Card.Text>
+                <Card.Text className="price">$15.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={salad} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+                <Card.Title>Scrumptious Salads</Card.Title>
+                <Card.Text className="date">Jan 9, 2020</Card.Text>
+                <Card.Text className="price">$20.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={guitar} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This card
-                  has even longer content than the first to show that equal height action.
-                </Card.Text>
+                <Card.Title>Acoustic Guitar Basics</Card.Title>
+                <Card.Text className="date">Jan 10, 2020</Card.Text>
+                <Card.Text className="price">$25.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+            </Card>
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={santorini} />
+              <Card.Body>
+                <Card.Title>A Day In Santorini</Card.Title>
+                <Card.Text className="date">Jan 12, 2020</Card.Text>
+                <Card.Text className="price">$10.00/person</Card.Text>
+              </Card.Body>
             </Card>
           </CardDeck>
         </div>
@@ -72,41 +76,37 @@ const MainActivities = () => {
         <h2>Aligned With You</h2>
         <div className="MainActivities-Aligned-Container">
           <CardDeck>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={pasta} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This
-                  content is a little bit longer.
-                </Card.Text>
+                <Card.Title>Cooking Italian Favourites</Card.Title>
+                <Card.Text className="date">Jan 8, 2020</Card.Text>
+                <Card.Text className="price">$35.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={baking} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+                <Card.Title>Baking With Grandma</Card.Title>
+                <Card.Text className="date">Jan 11, 2020</Card.Text>
+                <Card.Text className="price">$5.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={rock} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This card
-                  has even longer content than the first to show that equal height action.
-                </Card.Text>
+                <Card.Title>It's All About Rock N'Roll</Card.Title>
+                <Card.Text className="date">Jan 12, 2020</Card.Text>
+                <Card.Text className="price">$12.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+            </Card>
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={cocktail} />
+              <Card.Body>
+                <Card.Title>Ginspiration Cocktails</Card.Title>
+                <Card.Text className="date">Jan 14, 2020</Card.Text>
+                <Card.Text className="price">$15.00/person</Card.Text>
+              </Card.Body>
             </Card>
           </CardDeck>
         </div>
@@ -115,41 +115,37 @@ const MainActivities = () => {
         <h2>Try Something New</h2>
         <div className="MainActivities-New-Container">
           <CardDeck>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={broadway} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This
-                  content is a little bit longer.
-                </Card.Text>
+                <Card.Title>Sing Songs With A Broadway Star</Card.Title>
+                <Card.Text className="date">Jan 9, 2020</Card.Text>
+                <Card.Text className="price">$110.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={drag} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+                <Card.Title>Sangria & Secrets with Drag Queens</Card.Title>
+                <Card.Text className="date">Jan 30, 2020</Card.Text>
+                <Card.Text className="price">$15.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
             </Card>
-            <Card>
-              <Card.Img variant="top" src={} />
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={french} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to additional content. This card
-                  has even longer content than the first to show that equal height action.
-                </Card.Text>
+                <Card.Title>Learning French</Card.Title>
+                <Card.Text className="date">Jan 31, 2020</Card.Text>
+                <Card.Text className="price">$10.00/person</Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+            </Card>
+            <Card bg="card-theme">
+              <Card.Img variant="top" src={cocktail1} />
+              <Card.Body>
+                <Card.Title>Making Cocktails</Card.Title>
+                <Card.Text className="date">Jan 31, 2020</Card.Text>
+                <Card.Text className="price">$15.00/person</Card.Text>
+              </Card.Body>
             </Card>
           </CardDeck>
         </div>
